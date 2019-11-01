2c14413bae7b0b45c26e8520b4fd3f3dc1a20cf9
shizixingjin <1148042015@qq.com>
2019-11-01 22:10:30 +0800

    challenge

--- challenges/views.py
+++ challenges/views.py
@@ -13,8 +13,42 @@ from accounts import models as accounts_models
 def index(request):
     if not request.session.get('is_login', None):
         return redirect('/accounts/login/')
-    challenge = models.Challenges.objects.all()
+    # challenge = models.Challenges.objects.all()
+    challenge = models.Challenges.objects.order_by("point")
+    data_re = []
+    data_pwn = []
+    data_web = []
+    for c in challenge:
+        if c.category == 0:     # re
+            re = PassInsideView(c.name, c.category, c.message, c.point, c.file, c.flag, c.id)
+            data_re.append(re)
+        elif c.category == 2:   # pwn
+            p = PassInsideView(c.name, c.category, c.message, c.point, c.file, c.flag, c.id)
+            data_pwn.append(p)
+        elif c.category == 1:   # web
+            w = PassInsideView(c.name, c.category, c.message, c.point, c.file, c.flag, c.id)
+            data_web.append(w)
     return render(request, 'challenges_list.html', locals())
+
+
+class PassInsideView():
+    name = ''
+    category = ''
+    message = ''
+    point = ''
+    file = ''
+    flag = ''
+    id = ''
+
+    def __init__(self, name, category, message, point, file, flag, id):
+        self.name = name
+        self.category = category
+        self.message = message
+        self.point = point
+        self.file = file
+        self.flag = flag
+        self.id = id
+
 
 def solve(request, challenge_id):
     if not request.session.get('is_login', None):
@@ -25,41 +59,72 @@ def solve(request, challenge_id):
 
 
 class IndexView(View):
-    def get(self, request, challenge_id):
-        if not request.session.get('is_login', None):
-            return redirect('/accounts/login/')
-        form = flagForm()
-        challenge = models.Challenges.objects.get(id=challenge_id)
-
-        return render(request, 'solve.html', {'challenge': challenge, 'form': form})
+    # def get(self, request, challenge_id):
+    #     if not request.session.get('is_login', None):
+    #         return redirect('/accounts/login/')
+    #     form = flagForm()
+    #     challenge = models.Challenges.objects.get(id=challenge_id)
+    #
+    #     return render(request, 'solve.html', {'challenge': challenge, 'form': form})
 
     def post(self, request, challenge_id):
         if not request.session.get('is_login', None):
             return redirect('/accounts/login/')
-        form = flagForm(request.POST)
-        if form.is_valid():
-            flag = form.cleaned_data.get('flag')
-            challenge = models.Challenges.objects.get(id=challenge_id)
-            current_id = request.session['user_id']
-            current_user = accounts_models.User.objects.get(id=current_id)
-            # challenge.solver.all()
-
-            if challenge.solver.filter(id=current_id).exists():
-                return HttpResponse('have done')
-            else:
-                if flag == challenge.flag:
-                    solver = current_user
-                    challenge.solver.add(solver)
-                    current_user.point += challenge.point
-                    current_user.save()
-                    try:
-                        team = current_user.team
-                        team.point += challenge.point
-                        team.save()
-                    except:
-                        pass
-                    return HttpResponse('success')
-                else:
-                    return HttpResponse('wrong')
+        # print(request.POST)
+        flag = request.POST.get(str(challenge_id)+'_flag')
+        # print(flag)
+        challenge = models.Challenges.objects.get(id=challenge_id)
+        current_id = request.session['user_id']
+        current_user = accounts_models.User.objects.get(id=current_id)
+        if challenge.solver.filter(id=current_id).exists():
+            response = '<div id="flag_already"><p>ALREADY SUBMITTED</p></div>'
+            return HttpResponse(response)
         else:
-            return HttpResponse('fail')
+            if flag == challenge.flag:
+                solver = current_user
+                challenge.solver.add(solver)
+                current_user.point += challenge.point
+                current_user.save()
+                try:
+                    team = current_user.team
+                    team.point += challenge.point
+                    team.save()
+                except:
+                    pass
+                response = '<div id="flag_correct"><p>CORRECT</p></div>'
+                return HttpResponse(response)
+            else:
+                response = '<div id="flag_incorrect"><p>INCORRECT</p></div>'
+                return HttpResponse(response)
+
+
+        # form = flagForm(request.POST)
+        # if form.is_valid():
+        #     flag = form.cleaned_data.get('flag')
+        #     challenge = models.Challenges.objects.get(id=challenge_id)
+        #     current_id = request.session['user_id']
+        #     current_user = accounts_models.User.objects.get(id=current_id)
+        #     # challenge.solver.all()
+        #
+        #     if challenge.solver.filter(id=current_id).exists():
+        #         response = '<div id="flag_already"><p>ALREADY SUBMITTED</p></div>'
+        #         return HttpResponse(response)
+        #     else:
+        #         if flag == challenge.flag:
+        #             solver = current_user
+        #             challenge.solver.add(solver)
+        #             current_user.point += challenge.point
+        #             current_user.save()
+        #             try:
+        #                 team = current_user.team
+        #                 team.point += challenge.point
+        #                 team.save()
+        #             except:
+        #                 pass
+        #             response = '<div id="flag_correct"><p>CORRECT</p></div>'
+        #             return HttpResponse(response)
+        #         else:
+        #             response = '<div id="flag_incorrect"><p>INCORRECT</p></div>'
+        #             return HttpResponse(response)
+        # else:
+        #     return HttpResponse('fail')
